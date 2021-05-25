a660073e1e21ebe8126b1bac20a90a33e8ea9c35
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-05-25 02:08:25 +0200

    #1-Creado inicio para cambio de contraseña

--- src/app/_components/modal-change-pass/modal-change-pass.component.ts
+++ src/app/_components/modal-change-pass/modal-change-pass.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {MdbModalRef} from 'mdb-angular-ui-kit';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ValidatorsExtension} from '../../_helpers/validators-extension';
+import {ErrorService} from '../../_services/error.service';
+import {Toast} from '../../_models/toast';
+import {GimnasioService} from '../../_services/gimnasio.service';
+import {AuthService} from '../../_services/auth.service';
 
 @Component({
   selector: 'app-modal-change-pass',
@@ -14,7 +18,8 @@ export class ModalChangePassComponent implements OnInit {
   updatePass!: FormGroup;
 
   constructor(public modalRef: MdbModalRef<ModalChangePassComponent>,
-              private fb: FormBuilder) {
+              private fb: FormBuilder,
+              private auth: AuthService) {
   }
 
   ngOnInit(): void {
@@ -29,7 +34,26 @@ export class ModalChangePassComponent implements OnInit {
   }
 
   public async onSubmit(): Promise<void> {
+    if (this.updatePass.invalid) {
+      this.updatePass.markAllAsTouched();
+      return;
+    }
 
+    ErrorService.clean();
+
+    const inputs = this.updatePass.value;
+
+    const resultado: boolean = await this.auth.;
+
+    if (!resultado) {
+      ErrorService.showInForm(this.updatePass);
+    } else {
+      this.modalRef.close();
+      Toast.fire({
+        icon: 'success',
+        title: 'Contraseña cambiada con éxito'
+      }).then(_ => 0);
+    }
   }
 
 }
